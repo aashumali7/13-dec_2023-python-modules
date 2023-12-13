df9f5c7d7267966450c0e2f14d06a831c12d92f7
aashumali <aashishmali223@gmail.com>
2023-12-13 11:24:56 +0530

    new:

---
+++ aashish.py
@@ -0,0 +1,11 @@
+# Python speacific Import
+from aashu import greetEveryone1,greetEveryone5,Child   
+
+
+#2. function calling/invoking
+greetEveryone1()
+greetEveryone5()
+
+#create class object 
+co = Child() #class object = ClassName
+co.getMyBloodGroup() 
---
+++ aashu.py
@@ -0,0 +1,33 @@
+# function Defination
+def sayHello():
+    print("hello everyone")
+
+def greetEveryone1():
+    print("hey1")    
+def greetEveryone2():
+    print("hey2")    
+def greetEveryone3():
+    print("hey3")    
+def greetEveryone4():
+    print("hey4")    
+def greetEveryone5():
+    print("hey5")    
+
+#class defination
+class Parent():
+    #1.1 property 
+    bloodGroup='ab+ve'
+
+    #1.2 constructor
+
+    #1.3 method  
+ 
+  #class defination
+class Child(Parent):
+    #1.1 property
+
+    #1.2 constructor
+
+    #1.3 method
+    def getMyBloodGroup(self):
+        print(f'My Blood Group Is {self.bloodGroup}')
